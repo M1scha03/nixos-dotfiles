e59932094394de54a13e7cf28d637756e56dfd65
Mischa <295005705+M1scha03@users.noreply.github.com>
2026-06-23 00:28:23 +0200

    moved tmux to hm

--- config/nvim/plugin/50_colorschemes.lua
+++ config/nvim/plugin/50_colorschemes.lua
@@ -19,8 +19,8 @@ require("tokyonight").setup({
 require("catppuccin").setup({
 	transparent_background = true,
 	float = {
-		transparent = true, -- enable transparent floating windows
-		solid = true, -- use solid styling for floating windows, see |winborder|
+		transparent = true,
+		solid = true,
 	},
 })
 
